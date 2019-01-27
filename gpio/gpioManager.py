90c03e38de8bee2ca91e2a79ff94ec3a2d0ea4c7
Nicolas Bigler <nicolas.bigler@meiru.ch>
2019-01-27 20:44:50 +0000

    Doc

--- gpio/gpioManager.py
+++ gpio/gpioManager.py
@@ -12,11 +12,20 @@ from autobahn.wamp.exception import ApplicationError
 
 
 class Component(ApplicationSession):
+	"""Manages the gpio on the raspberry pi.
+	This class handles all the switching and reading from
+	the Raspberry pi gpio.
 
-	"""
-	Manages the gpio on the raspberry pi
+	The word "device" is used for all mechanical devices controlled
+	by the raspberry pi (pump, valve...).
 	"""
 	def initGPIO(self, pins):
+		"""Initialize the GPIO system.
+		Sets all pins to high (all devices are off)
+
+		Args:
+			pins (list): A list of all the gpio pins in use.
+		"""
 		print("initializing gpio")
 		print(pins)
 		GPIO.setmode(GPIO.BOARD)
@@ -24,18 +33,60 @@ class Component(ApplicationSession):
 			GPIO.setup(chan, GPIO.OUT, initial=GPIO.HIGH)
 
 	def isRunning(self, pin):
+		"""Checks if a device is being powered.
+
+		Args:
+			pin: The gpio pin on which the device of interest is connected.
+
+		Returns:
+			bool: True if the device is powered. False otherwise
+		"""
+
 		return not GPIO.input(pin) 
 
 	def turnOn(self, pin):
+		"""Start powering a device
+		Set the GPIO pin to low to trigger the relay.
+		Careful: This depends on the logic of the relay.
+		Args:
+			pin: The gpio pin on which the device is attached.
+		"""
 		GPIO.output(pin, GPIO.LOW)
 
 	def turnOff(self, pin):
+		"""Sops powering a device
+		Set the GPIO pin to high to trigger the relay.
+		Careful: This depends on the logic of the relay.
+		Args:
+			pin: The gpio pin on which the device is attached.
+		"""
 		GPIO.output(pin, GPIO.HIGH)
 
 	# Defining remote procedures
 	@inlineCallbacks
 	def switch(self, name):
+		"""Switches a specific device (on/off) if allowed
 
+		If the device is on, it will turn it off, and vice versa.
+		Before blindly switching, the function checks if this is
+		allowed or if it could be dangerous. The rules are as follow.
+		
+		If the device is a pump, then we can switch it on only if there
+		is at least one open valve in the same group. A pump can always be
+		switched off.
+
+		If the device is a valve, then we can switch it on or off only if
+		there is no pump running in the same group.
+
+		Args:
+			name: The name of the device.
+
+		Returns:
+			(string): "success" if we could switch the device.
+
+		Raises:
+			ApplicationError: If we were not allowed to switch the device.
+		"""
 		obj = yield self.call(u"ch.db.getobjdata", name)
 
 		shouldSwitch = False
