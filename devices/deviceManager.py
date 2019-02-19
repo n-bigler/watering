9febbf2d38bf3e3bc4e310e2b5312f37664d5fad
Nicolas Bigler <nicolas.bigler@meiru.ch>
2019-02-19 10:46:25 +0000

    clean up

--- devices/deviceManager.py
+++ devices/deviceManager.py
@@ -60,34 +60,28 @@ class Component(ApplicationSession):
 		running = yield self.isRunning(device["id"])
 
 		if device["type"] == "pump":
-			print("it's a pump")
 			if running == True: #turning off pump: safe
-				print("the pump is already running")
 				shouldSwitch=True
 			else: #need to check if there is a valve open in the group
-				print("pump not yet running")
 				groupList = yield self.call(u"ch.db.getobjgroup", device["group"])
 				for item in groupList:
 					res = yield self.isRunning(item["id"])
-					print(res)
 					if item["type"] == 'valve' and res == True:
-						print("found at least one open valve")
 						shouldSwitch = True
 
 		else: #it's a valve
-			print("it's a valve")
 			shouldSwitch=True
 			groupList = yield self.call(u"ch.db.getobjgroup", device["group"])
 			for item in groupList:
 				res = yield self.isRunning(item["id"])
 				if item["type"] == "pump" and res == True:
-					print("there is a running pump")
 					shouldSwitch = False
 
 		if shouldSwitch:
 			print("we should switch")
 			self.publish(u"ch.watering.logging", 
 				{'msg': 'Switching device {}'.format(device["name"]), 'level':'info'})
+		
 			res = yield self.call(u"ch.gpio.switch", device["id"])
 			return res
 
